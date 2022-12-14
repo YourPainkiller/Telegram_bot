d98dce6123aee634476fcd7a359f682e7c5416dc
Jyuty <72391777+Jyuty@users.noreply.github.com>
2022-12-14 15:11:30 +0300

    Update requests_f.py

--- requests_f.py
+++ requests_f.py
@@ -1,7 +1,11 @@
 import requests
 import json
 
+
 def symbol_to_name() -> dict:
+    """Функция создает словарь, где ключ это сокращенное название криптовалюты, а значение название криптовалюты.
+    На вход ничего не принимает. 
+    return - словарь"""
     m = {}
     request = "https://api.coincap.io/v2/assets"
     response = requests.get(request)
@@ -13,6 +17,9 @@ def symbol_to_name() -> dict:
     return m
 
 def name_to_symbol() -> dict:
+    """Функция создает словарь, где ключ это название криптовалюты, а значение сокращенное название криптовалюты.
+    На вход ничего не принимает. 
+    return - словарь"""
     m = {}
     request = "https://api.coincap.io/v2/assets"
     response = requests.get(request)
@@ -24,6 +31,9 @@ def name_to_symbol() -> dict:
     return m
 
 def get_list_of_crypto() -> str:
+    """Функция возвращает строку с всеми доступными назавниями криптовалюты для взаимодействия.
+    На вход ничего не принимает.
+    return - string"""
     request = "https://api.coincap.io/v2/assets"
     response = requests.get(request)
     text = response.json()
@@ -33,6 +43,9 @@ def get_list_of_crypto() -> str:
     return answer
 
 def get_current_price_of_crypto(crypto: str):
+    """Функция вывод цену криптовалюты crypto и ее изменение в процентах за 24 часа.
+    На вход принимает строку - название криптовалюты для которой нужно узнать цену.
+    return - string"""
     request = "https://api.coincap.io/v2/assets/" + crypto
     response = requests.get(request)
     text = response.json()
@@ -44,6 +57,9 @@ def get_current_price_of_crypto(crypto: str):
     return answer
 
 def best_place_to_buy_or_sell_crypto(crypto: str) -> list:
+    """Функция возвращает отсортированный по возрастанию цены массив, которой состоит из [название биржи][цена] для оптимальной покупки или продажи определенной криптовалюты
+    На вход принимает строку - название криптовалюты для которой нужно найти оптимальное место покупки/продажи.
+    return list[][]"""
     request = "https://api.coincap.io/v2/assets/" + crypto + "/markets"
     response = requests.get(request)
     text = response.json()
@@ -59,6 +75,9 @@ def best_place_to_buy_or_sell_crypto(crypto: str) -> list:
     return market_price_list
 
 def check_valid_crypto(crypto: str, symbol_from_name: dict, name_from_symbol: dict) -> bool:
+    """Функция проверяет валидность написания введенной криптовалюты.
+    На вход принимает название криптовалюты и два словаря с кратким значением криптовалюты и полным названием криптовалюты.
+    return bool"""
     crypto = crypto.lower()
     if (crypto in symbol_from_name or crypto in name_from_symbol):
         return True
